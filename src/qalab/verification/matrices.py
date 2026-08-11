2953121220d3e81f6fd1f40407ecce602077eba7
Moh-albataineh <albatainehm1@my.smccd.edu>
2026-08-10 20:55:15 -0700

    Add unitary matrix verification and tests

--- src/qalab/verification/matrices.py
+++ src/qalab/verification/matrices.py
@@ -22,4 +22,27 @@ def is_hermitian(matrix):
     conj = np.conj(matrix)
     dagger = conj.T
     return np.allclose(dagger , matrix)
+
+def is_unitary(matrix):
+    """
+    Checks if a matrix is unitary (U * U^dagger = Identity matrix).
     
+    Args:
+        matrix (numpy.ndarray): The input matrix to check.
+        
+    Returns:
+        bool: True if the matrix is unitary, False otherwise.
+        
+    Example:
+        >>> import numpy as np
+        >>> pauli_x = np.array([[0, 1], 
+        ...                     [1, 0]])
+        >>> is_unitary(pauli_x)
+        True
+    """
+    if matrix.ndim != 2 or matrix.shape[0] != matrix.shape[1]:
+        return False 
+    conj = np.conj(matrix)
+    dagger = conj.T
+    Unit = dagger @ matrix
+    return np.allclose(Unit , np.eye(matrix.shape[0]))
--- tests/test_matrices.py
+++ tests/test_matrices.py
@@ -1,7 +1,7 @@
 import numpy as np 
-from qalab.verification.matrices import is_hermitian
+from qalab.verification.matrices import is_hermitian , is_unitary
 
-#is hermitian 
+#is_hermitian 
 def test_is_hermitian_True():
     matrix = np.array([[1,0],
                        [0,-1]])
@@ -21,4 +21,31 @@ def test_is_hermitian_not_square():
     matrix = np.array([[1,2,3],
                        [4,5,6]])
     assert not is_hermitian(matrix)
+
+#is_unitary
+def test_is_unitary_True():
+    matrix = np.array([[1,1],[1,-1]]/np.sqrt(2))
+    assert is_unitary(matrix)
+
+def test_is_unitary_False():
+    matrix = np.array([[1,0],[0,2]])
+    assert not is_unitary(matrix)
+    
+def test_is_unitary_complex():
+    matrix = np.array([[0,-1j],
+                       [1j,0]])
+    assert is_unitary(matrix)
+    
+def test_is_unitary_not_square():
+    matrix = np.array([[1,2,3],
+                       [4,5,6]])
+    assert not is_unitary(matrix)
+
+def test_is_unitary_not_2x2():
+    matrix = np.array([[0, 1, 0], 
+                       [0, 0, 1], 
+                       [1, 0, 0]])
+    assert is_unitary(matrix)
+    
+    
     
